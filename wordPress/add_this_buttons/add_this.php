723f882ccc235021f0473f98d2c23ae265214ae5
Kay W <kaylaenojado@gmail.com>
2013-07-26 17:01:26 -0700

    JS & HTML into PHP variables

--- wordPress/add_this_buttons/add_this.php
+++ wordPress/add_this_buttons/add_this.php
@@ -6,7 +6,6 @@ Description: Vertical menu for AddThis social network share buttons
 License: GNU General Public License v2 or later
 */
 
-//I'm not sure if I got the right js script 
 
 function add_this_sharing_enqueue_script(){
 	    wp_enqueue_script(
@@ -20,28 +19,15 @@ function add_this_sharing_enqueue_script(){
 
 add_action( 'wp_enqueue_scripts', 'add_this_sharing_enqueue_script' );
 
-
-//This is my first attempt to use the Pimpletrest lecture and apply it to the AddThis sharing Plugin 
-
-function add_this_sharing( $content ) {
+function add_this_sharing_buttons( $content ) {
     if ( is_single() ) {
-        // Create the Sharing buttons HTML- Code borrowed from the pimpletrest example
-        $button_html  = '<a href="//pinterest.com/pin/create/button/" data-pin-do="buttonBookmark">';
-        $button_html .= '<img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" />';
-        $button_html .= '</a>';
-     
 
-        //This is the HTML borrowed from the AddThis website 
-
-        <div class="addthis_toolbox addthis_default_style ">
-		<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-		<a class="addthis_button_tweet"></a>
-		<a class="addthis_button_pinterest_pinit"></a>
-		<a class="addthis_counter addthis_pill_style"></a>
-		</div>
-		
-
-        // Append the buttons to the content
+        $button_html = '<div class="addthis_toolbox addthis_default_style ">';
+		$button_html .= '<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>';
+		$button_html .= '<a class="addthis_button_tweet"></a>';
+		$button_html .= '<a class="addthis_button_pinterest_pinit"></a>';
+		$button_html .= '<a class="addthis_counter addthis_pill_style"></a>';
+		$button_html .= '</div>';
         
         $content .= $button_html;
     }
@@ -49,5 +35,26 @@ function add_this_sharing( $content ) {
     return $content;
 }
 
-add_filter( 'the_content', 'add_this_sharing', 20 );
+function add_this_sharing_script( $footer ){
+
+        $js_html = '<script type="text/javascript">';
+        $js_html .= 'addthis.layers({';
+        $js_html .= '"theme" : "gray",';
+        $js_html .= '"share" : {';
+        $js_html .= '"position" : "right",';
+        $js_html .= '"numPreferredServices" : 3';
+        $js_html .= '},';
+        $js_html .= '"whatsnext" : {},';
+        $js_html .= '"recommended" : {';
+        $js_html .= '"title": "Recommended for you:"';
+        $js_html .= '}';
+        $js_html .= '});';
+        $js_html .= '</script>';   
+    
+        $footer .= $js_html;
+        return $footer;
+}
+
+add_filter( "the_content", "add_this_sharing_buttons", 20 );
+add_filter( "the_footer", "add_this_sharing_script", 21 );
 
